30006eb4a4f1f68f93b66cef835edb12cefe4f65
jaswanthreddy-007 <88081034+jaswanthreddy-007@users.noreply.github.com>
2023-03-14 15:24:16 +0530

    first commit

---
+++ src/pages/bgr.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function bgr() {
+    return (
+        <p>
+            Background removal will be done here
+        </p>
+    )
+}
---
+++ src/pages/file_comp.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function file_comp() {
+    return (
+        <p>
+            File compression will be done here
+        </p>
+    )
+}
